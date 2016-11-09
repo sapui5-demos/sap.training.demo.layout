ab73233cf524970ab3d99563a0a392ab6b09e9c9
kicker123 <chriskne@gmail.com>
2016-11-09 16:39:34 +0000

    New theme sap_belize

--- webapp/controller/Grid.controller.js
+++ webapp/controller/Grid.controller.js
@@ -1,44 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/Device"
+], function(Controller, Device) {
 	"use strict";
 
 	return Controller.extend("sap.training.controller.Grid", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf sap.training.view.Grid
-		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+			// apply compact density if touch is not supported, the standard cozy design otherwise
+			this.getView().addStyleClass(Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact");
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf sap.training.view.Grid
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+			Device.media.attachHandler(this._sizeStandardChanged, this, Device.media.RANGESETS.SAP_STANDARD);
+			Device.media.attachHandler(this._sizeExtendedChanged, this, Device.media.RANGESETS.SAP_STANDARD_EXTENDED);
+		},
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf sap.training.view.Grid
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		_sizeStandardChanged: function(mParams) {
+			var oMessageStrip = this.getView().byId("idStandardMessage");
+			oMessageStrip.setText("Rangeset SAP_STANDARD: " + mParams.name + ": From " + mParams.from + " " + mParams.unit + " to " + mParams.to +
+				" " + mParams.unit);
+		},
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf sap.training.view.Grid
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		_sizeExtendedChanged: function(mParams) {
+			var oMessageStrip = this.getView().byId("idExtendedMessage");
+			oMessageStrip.setText("Rangeset SAP_STANDARD_EXTENDED: " + mParams.name + ": From " + mParams.from + " " + mParams.unit + " to " +
+				mParams.to + " " + mParams.unit);
+		}
 
 	});
 
